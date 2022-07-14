8ff3b99c558316821a6cd3f97fb3af9f204d056d
Bryan Bosire <bryanbosire@gmail.com>
2022-07-14 22:19:37 +0300

    commit tree 5d06e554b827ddcc4fe2e5adf4fb4124f5241621

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (<div id="home">
+    {/* an <h1> element, with the style attribute set to a color of "firebrick" */}
+    <h1 style={{ color: "firebrick" }}>
+      {name} is a Web Developer from {city}
+    </h1>
+  </div>);
 }
 
 export default Home;
